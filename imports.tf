d2908dd8347386dc6314518f0221bc29979167b3
Andrey Takhtamirov <andrey1229@yahoo.com>
2024-07-17 19:01:33 -0400

    Setup initial project.

---
+++ imports.tf
@@ -0,0 +1,4 @@
+import {
+  to = google_compute_firewall.default_allow_ssh
+  id = "default-allow-ssh"
+}
---
+++ vars.tf
@@ -0,0 +1,5 @@
+variable "bucket_name" {
+  type        = string
+  description = "Bucket containing graphhopper config and data files."
+  default     = "trailblaze-graphhopper-bucket"
+}
